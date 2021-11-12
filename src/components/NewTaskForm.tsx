bb1c8578f8f5794aa26b72f87e98d1a42cf0be9b
krungy <furler711@gmail.com>
2021-11-12 15:43:10 +0900

    NewTaskForm 컴포넌트 추가

---
+++ src/components/NewTaskForm.tsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled"
+import { FormEvent, useState } from "react"
+import { useTasks } from "../contexts/TaskProvider"
+
+const NewTaskForm = (props: any) => {
+  const [task, setTask] = useState('')
+  const { addTask } = useTasks()
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    addTask(task)
+    setTask('')
+  }
+
+  return (
+    <Form {...props} onSubmit={handleSubmit}>
+      <Input 
+        type="text" 
+        value={task} 
+        onChange={e => setTask(e.target.value)} 
+        required 
+      />
+      <SumbitButton>Add</SumbitButton>
+    </Form>
+  )
+}
+
+export default NewTaskForm
+
+const Form = styled.form`
+  width: 400px;
+`
+
+const Input = styled.input`
+  width: 332px;
+  height: 32px;
+  padding: 4px 6px;
+  border-radius: 8px;
+  border: 2px solid black;
+  box-sizing: border-box;
+`
+const SumbitButton = styled.button`
+  width: 60px;
+  height: 32px;
+  padding: 4px 6px;
+  margin-left: 8px;
+  color: #fff;
+  border-radius: 8px;
+  border: none;
+  background-color: black;
+  box-sizing: border-box;
+  cursor: pointer;
+`
